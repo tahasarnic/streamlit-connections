5696b1f77e24b150edac4ed62893f6c2e7d19072
Taha Eren SARNIC <tahasarnic@gmail.com>
2023-07-25 22:21:32 +0300

    cached versiyonu yaparsak win

--- app.py
+++ app.py
@@ -1,4 +1,34 @@
 import streamlit as st
+import pandas as pd
+from json import loads
+from sentence_transformers import SentenceTransformer
 from qdrant_connection import QdrantConnection
+from qdrant_client import models
 
 conn = st.experimental_connection('qdrant_db', type=QdrantConnection)
+
+st.write(conn.get_collections())
+
+st.write(conn.cursor().get_collections())
+
+conn.create_collection(collection_name='new_collection', size = 384, distance_type = models.Distance.COSINE)
+
+df = pd.read_csv('complaints.csv')
+df = df.dropna()
+documents = loads(df.to_json(orient = 'records'))
+text = [{'text':doc['text']} for doc in documents]
+
+encoder = SentenceTransformer('all-MiniLM-L6-v2')
+
+@st.cache_data(ttl = 3600)
+def get_embeddings(text):
+    embedding = []
+    for i in range(len(text)):
+        embedding.append(encoder.encode(text[i]['text']).tolist())
+    return embedding
+
+
+conn.upload_vectors(collection_name='new_collection', vector = get_embeddings(text), documents = documents)
+
+myquery = encoder.encode("comcat is bad").tolist()
+st.dataframe(conn.query(collection_name='new_collection', query=myquery, limit = 5))
--- qdrant_connection/connection.py
+++ qdrant_connection/connection.py
@@ -1,12 +1,78 @@
 #---PACKAGES---
 from streamlit.connections import ExperimentalBaseConnection
-from qdrant_client import models, QdrantClient
+from streamlit.runtime.caching import cache_data
+from qdrant_client import QdrantClient, models
+import pandas as pd
 
 class QdrantConnection(ExperimentalBaseConnection[QdrantClient]):
+    # Connect to qdrant
     def _connect(self, **kwargs) -> QdrantClient:
-        if 'database' in kwargs:
-            db = kwargs.pop('database')
-        else:
+        """
+        Pass a local path for testing,
+        Pass a host and port for server connection,
+        Pass url and api key for cloud connection
+        """
+        if 'path' in list(self._secrets.keys()):
+            path = self._secrets['path']
+            return QdrantClient(path = path)
+        elif 'host' in list(self._secrets.keys()) and 'port' in list(self._secrets.keys()):
+            host = self._secrets['host']
+            port = self._secrets['port']
+            return QdrantClient(host = host, port = port)
+        elif 'url' in list(self._secrets.keys()) and 'api_key' in list(self._secrets.keys()):
             url = self._secrets['url']
             api_key = self._secrets['api_key']
-        return QdrantClient(url=url, api_key=api_key)
+            return QdrantClient(url = url, api_key = api_key)
+        else:
+            return QdrantClient(":memory:")
+    
+    # Cursor for using qdrant client methods
+    def cursor(self) -> QdrantClient:
+        return self._instance
+    
+    # Viewing created collections
+    def get_collections(self):
+        return self._instance.get_collections()
+    
+    # Creating a collection
+    def create_collection(self, collection_name: str, size: int, distance_type = models.Distance.COSINE):
+        self._instance.recreate_collection(
+	        collection_name = collection_name,
+	        vectors_config = models.VectorParams(
+		    size = size, # Vector size is defined by used model
+		    distance = distance_type
+	        )
+        )
+
+    # Uploading vectors to collection
+    def upload_vectors(self, collection_name: str, vector: list = None, documents: list = None):
+        self._instance.upload_records(
+	        collection_name = collection_name,
+	        records=[
+		    models.Record(
+			    id = idx,
+			    vector = vec,
+			    payload = doc
+		    ) for idx, (doc, vec) in enumerate(zip(documents, vector))
+	            ]
+        )
+
+    # Query
+    def query(self, collection_name: str, query: list, limit: int = 5, **kwargs) -> pd.DataFrame:
+        hits = self._instance.search(
+            collection_name = collection_name,
+            query_vector = query,
+            limit = limit
+        )
+        results = []
+        for i in range(len(hits)):
+            results.append(hits[i].payload)
+        
+        return pd.DataFrame(results)
+    
+
+    
+
+    
+
+        
